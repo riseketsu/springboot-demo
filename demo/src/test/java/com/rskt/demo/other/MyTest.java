dd44c3d44ee582a308bb28642602b1a205a1b8ab
riseketsu <riseketsu@163.com>
2018-09-21 18:01:20 +0800

    upt

---
+++ demo/src/test/java/com/rskt/demo/other/MyTest.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright © 2018 RSKT. All rights reserved. 
+ */
+package com.rskt.demo.other;
+
+/**
+ * Class:MyTest.java<br>
+ *
+ * Class Summary：<br>
+ * 
+ * Class Feature:<br>
+ *
+ * @Author SHIJIELI
+ * @CreateDate 2018-09-13
+ *
+ */
+public class MyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
